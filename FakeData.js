0de68413ea71868bad7d415b213ab5cbd612e6f2
hemanthsaidev15 <143858304+hemanthsaidev15@users.noreply.github.com>
2023-12-05 13:12:28 +0530

    Add files via upload
    crud app using redux

---
+++ FakeData.js
@@ -0,0 +1,27 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Hemanth sai",
+      username: "hemanth",
+    },
+    {
+      id: 2,
+      name: "Goutami",
+      username: "Sony",
+    },
+    {
+      id: 3,
+      name: "Ravi kumar",
+      username: "Ravi",
+    },
+    {
+      id: 4,
+      name: "Hima charan",
+      username: "Hima",
+    },
+    {
+      id: 5,
+      name: "tarak",
+      username: "tak",
+    },
+  ];
